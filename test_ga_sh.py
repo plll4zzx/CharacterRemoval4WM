4798ea9cb6ae8cb786e1bba2a0df92f85f19de1c
plll4zzx <zhaoxi_zhang@163.com>
2025-02-08 23:40:24 +1000

    update

--- test_ga_sh.py
+++ test_ga_sh.py
@@ -2,6 +2,7 @@
 from textattack.utils import load_json
 import os
 from test_ga_attack import test_ga_attack
+import argparse
 
 def get_key_value(x_dict, key1, key2=None):
     if key2 is not None:
@@ -15,12 +16,22 @@ def get_key_value(x_dict, key1, key2=None):
 
 sh_templte='python test_ga_attack.py --num_generations {num_generations} --max_edit_rate {max_edit_rate} --len_weight {len_weight} --eva_thr {eva_thr} --fitness_threshold {fitness_threshold} --max_token_num {max_token_num} --victim_tokenizer "{victim_tokenizer}" --victim_model "{victim_model}" --wm_name "{wm_name}"'
 
-llm_name='facebook/opt-1.3b'
-ga_config=load_json(file_path='attk_config/opt_ga_config.json')
+# python test_ga_sh.py --llm_name "facebook/opt-1.3b" --wm_name "UPV"
+# python test_ga_sh.py --llm_name "../model/Llama3.1-8B_hg" --wm_name "UPV"
+parser = argparse.ArgumentParser(description='test_ga_attack')
+parser.add_argument('--llm_name', type=str, default='facebook/opt-1.3b')
+parser.add_argument('--wm_name', type=str, default='SynthID')
+args = parser.parse_args()
+
+llm_name=args.llm_name
+if 'opt' in llm_name:
+    ga_config=load_json(file_path='attk_config/opt_ga_config.json')
+else:
+    ga_config=load_json(file_path='attk_config/llama_ga_config.json')
 max_token_num_list=[100,200]#
 
 for max_token_num in max_token_num_list:
-    for wm_name in ['UPV']:#rand_config:
+    for wm_name in [args.wm_name]:#rand_config:
         wm_config=ga_config[wm_name]
         victim_tokenizer=wm_config['victim_tokenizer']
         victim_model=wm_config['victim_model']
